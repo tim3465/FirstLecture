d33fa812e61e64e084a98202fabaa4626b61232e
Tim Montague <tim.john.montague@gmail.com>
2023-07-10 11:39:26 -0400

    first

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Security;
+
+int assign = 0;
+Console.WriteLine(assign);
+assign = 5;
+Console.WriteLine(assign);
+assign += 2;
+Console.WriteLine(assign);
+assign -= 5;
+Console.WriteLine(assign);
+string catName = null;
+Console.WriteLine(catName);
+catName = "Felony";
+Console.WriteLine(catName);
+
+string y = null;
+if (y ==null)
+{
+    y = "dffsjjkfjjld";
+}
+
+//nonnullable
+//intz =null;
