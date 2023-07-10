13ee93129b89bb0c495fb23981f6ca316cbab174
Tim Montague <tim.john.montague@gmail.com>
2023-07-10 13:41:12 -0400

    added exersises

--- Program.cs
+++ Program.cs
@@ -1,23 +1,58 @@
-﻿using System.Security;
+﻿//using System.Security;
 
-int assign = 0;
-Console.WriteLine(assign);
-assign = 5;
-Console.WriteLine(assign);
-assign += 2;
-Console.WriteLine(assign);
-assign -= 5;
-Console.WriteLine(assign);
-string catName = null;
-Console.WriteLine(catName);
-catName = "Felony";
-Console.WriteLine(catName);
+//int assign = 0;
+//Console.WriteLine(assign);
+//assign = 5;
+//Console.WriteLine(assign);
+//assign += 2;
+//Console.WriteLine(assign);
+//assign -= 5;
+//Console.WriteLine(assign);
+//string catName = null;
+//Console.WriteLine(catName);
+//catName = "Felony";
+//Console.WriteLine(catName);
 
-string y = null;
-if (y ==null)
-{
-    y = "dffsjjkfjjld";
-}
+//string y = null;
+//if (y ==null)
+//{
+//    y = "dffsjjkfjjld";
+//}
 
 //nonnullable
 //intz =null;
+
+//EXERCISES
+
+//1
+//Console.WriteLine("ENTER a string");
+//string enter = Console.ReadLine();
+//Console.WriteLine(enter);
+
+//2
+Console.WriteLine("enter a number");
+string numberString = Console.ReadLine();
+double number = double.Parse(numberString);
+number++;
+Console.WriteLine(number);
+
+//3
+//Console.WriteLine("ENTER  a number");
+//double number = double.Parse(Console.ReadLine());
+//number = number + .5;
+//Console.WriteLine(number);
+//4
+//Console.WriteLine(" ENTER first number to add ");
+//double number = double.Parse(Console.ReadLine());
+//Console.WriteLine("ENTER sencond number to add");
+//double result = double.Parse(Console.ReadLine()) + number;
+//Console.WriteLine("anser is " + result);
+////5
+//using System.Threading.Channels;
+
+//Console.WriteLine(" ENTER A NUMBER");
+//double number = double.Parse(Console.ReadLine());
+//Console.WriteLine("ENTER A NUMBER TO Multiply ");
+//string stringSecondNumber = Console.ReadLine();
+//number = number * (double.Parse(stringSecondNumber));
+//Console.WriteLine(number);
